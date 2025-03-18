ce2668e33bd470ff1d7eaa4a2a992bc08e491e0a
Rubén García-Patos Benito <ruben.gbenito@alumnos.upm.es>
2025-03-18 12:45:53 +0100

    feat: current absolute line

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -11,6 +11,7 @@ vim.g.have_nerd_font = true
 --  For more options, you can see `:help option-list`
 
 -- Use relative numbers
+vim.opt.number = true
 vim.opt.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
