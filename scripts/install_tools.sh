dc5d729a3045735bbe30d3e92844c3dbcfc11225
Rubén García-Patos Benito <ruben.gbenito@alumnos.upm.es>
2025-06-02 14:42:54 +0200

    feat: added more tools

--- scripts/install_tools.sh
+++ scripts/install_tools.sh
@@ -3,4 +3,19 @@
 # brave
 curl -fsS https://dl.brave.com/install.sh | sh
 
-# 
+# asdf
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.14.0
+
+source ~/.zshrc
+
+# python, c, c++, java 
+# fedora included it
+
+# install elixir
+asdf plugins add erlang
+asdf plugins add elixir
+asdf install erlang 27.3.4
+asdf install elixir 1.18.4-otp-27
+
+# install haskell
+sudo dnf install ghc cabal-install
