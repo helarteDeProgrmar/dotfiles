c83b686d110d8a6d02b2a561fdead8aafa68d455
Rubén García-Patos Benito <ruben.gbenito@alumnos.upm.es>
2025-07-12 18:18:18 +0200

    feat: added system shortcuts

--- scripts/load-gnome-config.sh
+++ scripts/load-gnome-config.sh
@@ -1,2 +1,3 @@
-dconf load /org/gnome/desktop/ < shortcuts.txt
+dconf load /org/gnome/desktop/ < windows.txt
 dconf load /org/gnome/shell/extensions/ < extension.txt
+dconf load /org/gnome/settings-daemon/plugins/ < shortcuts.txt
--- scripts/update-gnome-config.sh
+++ scripts/update-gnome-config.sh
@@ -1,3 +1,4 @@
 # https://flathub.org/apps/com.mattjakeman.ExtensionManager
-dconf dump /org/gnome/desktop/ > shortcuts.txt
+dconf dump /org/gnome/desktop/ > windows.txt
 dconf dump /org/gnome/shell/extensions/ > extension.txt
+dconf dump /org/gnome/settings-daemon/plugins/ > shortcuts.txt
