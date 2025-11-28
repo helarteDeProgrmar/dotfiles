71156099951518ec5294af8bdf7bd557df6383f4
Rubén García-Patos Benito <ruben.gbenito@alumnos.upm.es>
2025-11-28 16:23:37 +0100

    feat: update gnome config

---
+++ wallpapers/gnome.sh
@@ -0,0 +1,2 @@
+gsettings set org.gnome.desktop.background picture-uri 'file:///home/rgpb/dotfiles/wallpapers/gnome-lune-dark.jpg'
+gsettings set org.gnome.desktop.background picture-uri-dark 'file:///home/rgpb/dotfiles/wallpapers/gnome-lune-dark.jpg'
